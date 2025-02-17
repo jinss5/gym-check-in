6fdc42f845a50fb2c0d830b6849112ba97783604
jinss5 <jinss1145@gmail.com>
2025-02-17 15:19:50 -0600

    add: tab bar icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,44 @@
 import { Tabs } from 'expo-router';
 import { CheckInProvider } from '../context/CheckInContext';
 import Toast from 'react-native-toast-message';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Layout() {
   return (
     <CheckInProvider>
-      <Tabs>
-        <Tabs.Screen name="index" options={{ title: 'Home' }} />
-        <Tabs.Screen name="calendar" options={{ title: 'Calendar' }} />
-        <Tabs.Screen name="stats" options={{ title: 'Stats' }} />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="calendar"
+          options={{
+            title: 'Calendar',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="calendar" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="stats"
+          options={{
+            title: 'Stats',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="stats-chart" size={24} color={color} />
+            ),
+          }}
+        />
       </Tabs>
       <Toast />
     </CheckInProvider>
